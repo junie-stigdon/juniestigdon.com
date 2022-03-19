8df54b17f5567f5badd0d9b3e3ff5b35fde1b3a6
Junie Senthilkumar <vanmathysk@gmail.com>
2022-03-19 16:01:09 -0600

    created landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'junie-stigdon';
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  faEnvelope = faEnvelope;
 }
